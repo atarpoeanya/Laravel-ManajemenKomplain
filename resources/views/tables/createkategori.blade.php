af077b900f0b742979a334dd5783870bc3bc901b
Belajar Git <belajargit@student.uns.ac.id>
2021-06-14 00:32:05 +0700

    edit
    edit data

--- resources/views/tables/createkategori.blade.php
+++ resources/views/tables/createkategori.blade.php
@@ -1,22 +1,25 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <table class="table">
-        <thead class="table-dark">
-            <tr>
-                <th scope="col">#</th>
-                <th scope="col">Nama</th>
-            </tr>
-        </thead>
-        <tbody>
-        @foreach($kategori as $value)
-            <tr>
-                <th scope="row">{{$loop->iteration}}</th>
-                <td>{{$value->nama}}</td>
-            </tr>
-        @endforeach
-        </tbody>
-    </table>
-</div>
+<form action="/kategori" method="POST">
+    @csrf
+
+    <label for="id_unit">Nama</label>
+    <select class="form-control @error('id_unit_parent') is-invalid @enderror" name="id_unit" id="id_unit" > 
+    @foreach($refunit as $value)
+        <option value="{{$value->id}}"> {{$value->nama}} </option>
+    @endforeach
+        
+    </select>
+        
+    <input type="hidden" class="form-control @error('created_by') is-invalid @enderror" 
+        id="created_by"  name="created_by" value="{{Auth::user()->name}}" readonly>
+
+    @error('nama')
+    <div class="invalid-feedback">{{ 'error' }}</div>
+    @enderror
+    <button type="submit">submit</button>
+
+</form>
+
 @endsection
